d5524e45831004e63c0ce08315fe7be05d93f84b
coderlore <coderlore@gmail.com>
2022-06-07 22:26:13 -1000

    Added delete, post, and morgan

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 const PORT = 3000
+
+app.use(express.json())
+app.use(morgan('tiny'))
+
+morgan.token('object', function (req, res) { 
+  return `${JSON.stringify(req.body)}`
+})
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :object'))
 
 let persons = [
     { 
@@ -22,29 +31,63 @@ let persons = [
       "id": 4,
       "name": "Mary Poppendieck", 
       "number": "39-23-6423122"
-    },
-    {  
-      "id": "unknown",
-      "name": "unknown",
-      "number": "unknown"
     }
 ]
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
-  })
+  response.json(persons)
+})
 app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id) - 1
-  if( persons[id] ){
-    response.json(persons[id]) 
+  const id = Number(request.params.id)
+  const entry = persons.find(entry => entry.id == id)
+  if(entry){
+    response.json(entry) 
   }else{
-    response.json(persons['unknown'])
+    response.status(404).end()
   }
-  })
+})
 app.get('/info', (request, response) => {
     const currentDate = new Date()
     response.send(`<h2>Phonebook has info for ${persons.length} people</h2> <h2>${currentDate}</h2>`)
-  })
-  app.listen(process.env.PORT || PORT, ()=>{
+})
+
+const generateId = () => {
+  const maxId = persons.length > 0
+    ? Math.max(...persons.map(n => n.id))
+    : 0
+  return maxId + 1
+}
+
+app.post('/api/persons/', (request, response) => {
+  const body = request.body
+  
+  if (!body.name){
+    return response.status(400).json({error: 'name is missing'}).end()
+  }
+
+  if (!body.number){
+    return response.status(400).json({error: 'number is missing'}).end()
+  }
+
+  if (persons.some(entry => entry.name === body.name)) {
+    return response.status(409).json({error: 'duplicate name'}).end()
+  }
+
+  let entry = {
+    id: generateId(),
+    name: body.name,
+    number: body.number
+  }
+  
+  persons.push(entry)
+  response.json(entry)
+})
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+  
+  response.status(204).end()
+})
+app.listen(process.env.PORT || PORT, ()=>{
     console.log(`The server is running on port ${PORT}! Betta Go Catch It!`)
 })
